9b35092448ed456548301217c883ec484e2b762a
Carlos Dourado <carlosismaelad@outlook.com>
2025-04-16 15:17:16 -0300

    creates a OpenAiService mock - for now

---
+++ src/main/java/com/carlosdourado/translatorapi/application/services/openAi/OpenAiService.java
@@ -0,0 +1,13 @@
+package com.carlosdourado.translatorapi.application.services.openAi;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class OpenAiService {
+
+    public String detectLocale(String content) {
+        // Integração com OpenAI API para detectar idioma
+        return "en-US";
+    }
+
+}
