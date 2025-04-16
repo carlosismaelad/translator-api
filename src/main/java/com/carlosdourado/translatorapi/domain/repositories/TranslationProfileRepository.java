6d2719c686888b3949ce8a832daab5c7f42c167a
Carlos Dourado <carlosismaelad@outlook.com>
2025-04-16 16:46:59 -0300

    adjust the return type in the repository in case the translator has more than one translation profile saved

--- src/main/java/com/carlosdourado/translatorapi/domain/repositories/TranslationProfileRepository.java
+++ src/main/java/com/carlosdourado/translatorapi/domain/repositories/TranslationProfileRepository.java
@@ -3,9 +3,9 @@ package com.carlosdourado.translatorapi.domain.repositories;
 import com.carlosdourado.translatorapi.domain.entities.TranslationProfile;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 public interface TranslationProfileRepository extends JpaRepository<TranslationProfile, UUID> {
-    Optional<TranslationProfile> findByTranslatorId(UUID id);
+    List<TranslationProfile> findByTranslatorId(UUID id);
 }
