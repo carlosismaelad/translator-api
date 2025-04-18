eaccd8b2d4266693c3ebf35472ed963369cb23e8
Carlos Dourado <carlosismaelad@outlook.com>
2025-04-18 18:29:42 -0300

    adjust the calling and usage of credentials.json

--- translator/translator-api/src/main/java/com/carlosdourado/translatorapi/application/services/googleTranslate/GoogleTranslateService.java
+++ translator/translator-api/src/main/java/com/carlosdourado/translatorapi/application/services/googleTranslate/GoogleTranslateService.java
@@ -1,5 +1,6 @@
 package com.carlosdourado.translatorapi.application.services.googleTranslate;
 
+import com.carlosdourado.translatorapi.application.exceptions.CredentialsNotFoundException;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.cloud.translate.Translate;
 import com.google.cloud.translate.TranslateOptions;
@@ -7,33 +8,32 @@ import com.google.cloud.translate.Translation;
 import org.springframework.stereotype.Service;
 
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.StandardCharsets;
 
 @Service
 public class GoogleTranslateService {
 
     private final Translate translate;
 
-    private static final String GOOGLE_CREDENTIALS_PATH = "/home/carlos-dourado/www/java-projects/translator-api/credentials.json";
-
     public GoogleTranslateService() {
         try {
-            File credentialsPath = new File(GOOGLE_CREDENTIALS_PATH);
-            if (credentialsPath.exists()) {
-                GoogleCredentials credentials = GoogleCredentials.fromStream(new FileInputStream(credentialsPath));
-                TranslateOptions options = TranslateOptions.newBuilder()
-                        .setCredentials(credentials)
-                        .build();
-                this.translate = options.getService();
-            } else {
-                throw new RuntimeException("Credenciais não encontradas: " + GOOGLE_CREDENTIALS_PATH);
+
+            String credentialsJson = System.getenv("GOOGLE_CREDENTIALS_JSON");
+
+            if (credentialsJson == null || credentialsJson.isEmpty()) {
+                throw new CredentialsNotFoundException("Variável de ambiente GOOGLE_CREDENTIALS_JSON não está definida!");
             }
-        } catch (IOException e) {
-            throw new RuntimeException("Erro ao carregar as credenciais do Google: " + e.getMessage(), e);
+
+            InputStream credentialsStream = new ByteArrayInputStream(credentialsJson.getBytes(StandardCharsets.UTF_8));
+
+            GoogleCredentials credentials = GoogleCredentials.fromStream(credentialsStream);
+            this.translate = TranslateOptions.newBuilder()
+                    .setCredentials(credentials)
+                    .build()
+                    .getService();
         } catch (Exception e) {
-            throw new RuntimeException("Erro inesperado ao criar o GoogleTranslateService: " + e.getMessage(), e);
+            throw new CredentialsNotFoundException("Erro ao carregar as credenciais do Google Translate.");
         }
     }
 
