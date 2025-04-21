745b9088e88221ca636f6ff83b2c94cb5830b2c5
Carlos Dourado <carlosismaelad@outlook.com>
2025-04-20 21:08:01 -0300

    fix SecurityConfig allowed origins

--- src/main/java/com/carlosdourado/translatorapi/infra/config/SecurityConfig.java
+++ src/main/java/com/carlosdourado/translatorapi/infra/config/SecurityConfig.java
@@ -73,7 +73,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOriginPatterns(List.of(
                 "https://translator-front.vercel.app",
-                "https://translator-front-3xd6.vercel.app/login"
+                "https://translator-front-3xd6.vercel.app"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
